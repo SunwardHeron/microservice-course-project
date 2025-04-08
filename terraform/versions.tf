8aee29647c2d48fcd6cfbd73c1c66fb06a719904
SunwardHeron <nhoj.ecrof@gmail.com>
2025-04-08 17:52:58 -0400

    Created versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+  default_tags {
+    tags = "jforce"
+  }
+}
